320e2f8d3244766dedee7333c2a91d471312140c
Eric <eric.christian.costa@gmail.com>
2022-01-02 10:38:28 -0300

    Components folder added to project

---
+++ components/Feed.jsx
@@ -0,0 +1,16 @@
+import Stories from "./Stories"
+
+const Feed = () => {
+    return (
+        <div>
+            <div>
+                <Stories />
+                {/* <InputBox /> */}
+                {/* <Posts /> */}
+            </div>
+            
+        </div>
+    )
+}
+
+export default Feed
